8054b8e9175cbafdd864820836b104645a8fbb44
Michael Tomasik <mstomasik@gmail.com>
2018-12-23 11:57:42 +0100

    added serialprint for testing

--- PitPump_Active.ino
+++ PitPump_Active.ino
@@ -1,6 +1,6 @@
 //Controls pump in oil pit
 
-
+//All pins need pull down resistors
 pumpLowLevelPin = 3;
 pumpHighLevelPin = 4;
 pumpRelayPin = 5;
@@ -11,43 +11,61 @@ unsigned long allowRunTime = 50000;
 unsigned long pumpRestTime = 100000;
 unsigned long pumpRestStart = 0;
 
-boolean pumpRunning = FALSE;    //True =pump running, False = pump off
-boolean pumpRestMode = False;   //True = resting,  False = Active
+boolean pumpRunning = false;    //True =pump running, False = pump off
+boolean pumpRestMode = true;   //True = resting,  False = Active
 
 void setup(){
   pinMode(pumpLowLevelPin, INPUT);  //monitors low water level float switch
   pinMode(pumpHighLevelPin), INPUT);//monitors high water level float switch
   pinMode(pumpRelayPin, OUTPUT);    //activates relay to turn pump on
+  pinMode(LED_BUILTIN, OUTPUT)      //activates builtin for testing purposes
+
+  Serial.println("*************Values at Setup***************")
+  Serial.println("currentTime = ", currentTime);
+  Serial.println("PumpStartTime = ", PumpStartTime);
+  Serial.println("allowRunTime = ", allowRunTime);
+  Serial.println("pumpRestTime = ", pumpRestTime);
+  Serial.println("pumpRestStart = ", pumpRestStart);
+  Serial.println("pumpRunning = ", pumpRunning);
+  Serial.println("pumpRestMode = ", pumpRestMode);
 }
 
 void loop() {
-
-
 
   currentTime = millis();
 
     //Skips pump control while pump is resting
     if (currentTime - pumpRestStart => pumpRestTime) {
-      pumpRestMode = FALSE;
+      pumpRestMode = false;
+
+    if (pumpRestMode = !pumpRestMode) {  //Test
+      Serial.println("Change of pump rest state.  It is now= ",pumpRestMode);
+    }
 
       //Turns pump off if run time exceeded while pump is running
-      if (currentTime - PumpStartTime >= allowRunTime  && pumpRunning == TRUE) {
+      if (currentTime - PumpStartTime >= allowRunTime  && pumpRunning == true) {
         digitalWrite(pumpRelayPin, HIGH);
         pumpRestMode = HIGH;
         pumpRestStart = millis();
+        digitalWrite(LED_BUILTIN, LOW);  //Test
+        Serial.println("Pump turned off due to exceeding time limit");   //Test
       }
 
       //Turns pump on if high water float closed and pump is previously off, resets pump timer
-      if (pumpHighLevelPin == HIGH && pumpRunning == FALSE) {
+      if (pumpHighLevelPin == HIGH && pumpRunning == false) {
         digitalWrite(pumpRelayPin, LOW);
-        pumpRunning = TRUE;
+        pumpRunning = true;
         PumpStartTime = millis();
+        digitalWrite(LED_BUILTIN, HIGH);  //Test
+        Serial.println("Pump turned on by high water level float");   //Test
       }
 
       //Turns off pump when low water level float (wired empty to OPEN) open
       if (pumpLowLevelPin == LOW) {
-        digitWrite (pumpRelayPin, HIGH);
-        pumpRunning= FALSE;
+        digitalWrite (pumpRelayPin, HIGH);
+        pumpRunning= false;
+        digitalWrite(LED_BUILTIN, LOW);  //Test
+        Serial.println("Pump turned off by low level float"); //Test
       }
   }
 }
